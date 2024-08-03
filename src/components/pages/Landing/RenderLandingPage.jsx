beb99d506f951a7029869f551afc02ba2d27c772
Sterling Sanders <sterlingsanders723@gmail.com>
2024-08-02 18:32:52 -0700

    Sprint 1 landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,6 +32,20 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
+      <div className="graphs-section">
+        <div className="graph-container">
+          <img src={GrantRatesByOfficeImg} alt="Grant Rates By Office" className="graph-img" />
+          <p className="graph-description">Grant Rates By Office</p>
+        </div>
+        <div className="graph-container">
+          <img src={GrantRatesByNationalityImg} alt="Grant Rates By Nationality" className="graph-img" />
+          <p className="graph-description">Grant Rates By Nationality</p>
+        </div>
+        <div className="graph-container">
+          <img src={GrantRatesOverTimeImg} alt="Grant Rates Over Time" className="graph-img" />
+          <p className="graph-description">Grant Rates Over Time</p>
+        </div>
+      </div>
       {/* <div className="graphs-section"> */}
       <div className="view-more-data-btn-container">
         <Button
@@ -41,7 +55,17 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF', marginLeft: '10px' }}
+          onClick={() => {
+            // logic to download the data
+          }}
+        >
+          Download the Data
+        </Button>
       </div>
+
 
       <div className="middle-section">
         <div className="hrf-img-container">
@@ -61,12 +85,34 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+        <div className="systemic-container">
+        <h1>Systemic Disparity Insights</h1>
+        </div>
+        
+        <div className="bottom-section">
+        <div className="insights-container">
+          
+          <div className="stat-item">
+            <h3>36%</h3>
+            <p>By the end of the Trump Administration, the average asylum office grant rate had fallen 36 percent from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 2020.</p>
+          </div>
+          <div className="stat-item">
+            <h3>5%</h3>
+            <p>The New York asylum office grant rate dropped to 5 percent in the fiscal year 2020.</p>
+          </div>
+          <div className="stat-item">
+            <h3>6x Lower</h3>
+            <p>Between fical year 2017 and 2020, the New York asylum office's average grant rate was six times lower than the San Francisco asylum office.</p>
+          </div>
+        
+        </div>
+      </div>
         {/* <div className="bottom-section">*/}
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
       </div>
-    </div>
+    </div >
   );
 }
 export default RenderLandingPage;
